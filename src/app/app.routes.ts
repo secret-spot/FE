72b6c599b1e8b1a5cada6c7f5ea73533c916c957
leejieun <mariewldms@naver.com>
2025-04-02 17:48:27 +0900

    Chore: initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { SplashComponent } from './pages/splash/splash.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { ChatbotComponent } from './pages/chatbot/chatbot.component';
+import { MypageComponent } from './pages/mypage/mypage.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'chatbot', component: ChatbotComponent },
+  { path: 'mypage', component: MypageComponent },
+];
---
+++ src/app/pages/splash/splash.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash',
+  imports: [],
+  templateUrl: './splash.component.html',
+  styleUrl: './splash.component.scss'
+})
+export class SplashComponent {
+
+}
