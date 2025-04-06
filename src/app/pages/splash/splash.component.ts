d5854d02278075316ceec9b1e4f7ccf2a3c6e8fb
leejieun <mariewldms@naver.com>
2025-04-06 14:40:26 +0900

    Feat: splash 리다이렉트

--- src/app/pages/splash/splash.component.ts
+++ src/app/pages/splash/splash.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -14,10 +14,7 @@ import { environment } from '../../../environments/environment';
 export class SplashComponent implements OnInit {
   showLoginButton = false;
 
-  constructor(
-    private router: Router,
-    private http: HttpClient
-  ) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     // 3초 후에 로그인 버튼 표시
@@ -27,25 +24,6 @@ export class SplashComponent implements OnInit {
   }
 
   signInWithGoogle() {
-    this.http.get(`${environment.apiUrl}/oauth2/authorization/google`, {
-      observe: 'response',
-      withCredentials: true
-    }).subscribe({
-      next: (response) => {
-        // 백엔드에서 리다이렉트 URL을 받아서 처리
-        if (response.headers.get('Location')) {
-          window.location.href = response.headers.get('Location')!;
-        } else {
-          // 응답 본문에 리다이렉트 URL이 있는 경우
-          const responseBody = response.body as any;
-          if (responseBody && responseBody.redirectUrl) {
-            window.location.href = responseBody.redirectUrl;
-          }
-        }
-      },
-      error: (error) => {
-        console.error('OAuth2 요청 실패:', error);
-      }
-    });
+    window.location.href = `${environment.apiUrl}/oauth2/authorization/google`;
   }
 } 
