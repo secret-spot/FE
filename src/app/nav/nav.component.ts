e0dbcd0b912c0a6b8beda3c29cea4e5bd58a02e4
mariewldms <143923436+mariewldms@users.noreply.github.com>
2025-04-25 18:43:59 +0900

    Feat: 네비게이션 svg 삽입

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,5 +9,9 @@ import { RouterModule } from '@angular/router';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  constructor(private router: Router) {}
 
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
