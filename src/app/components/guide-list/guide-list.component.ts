81957d1017f5f51a1dab9252a69d1a4b7c57e1e5
mariewldms <143923436+mariewldms@users.noreply.github.com>
2025-05-04 14:26:31 +0900

    Refactor: 컴포넌트화 폴더 생성

---
+++ src/app/components/guide-list/guide-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guide-list',
+  imports: [],
+  templateUrl: './guide-list.component.html',
+  styleUrl: './guide-list.component.scss'
+})
+export class GuideListComponent {
+
+}
